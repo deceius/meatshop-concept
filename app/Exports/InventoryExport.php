8d62a3f89ff574465606ca098b1b2dccbeba1cfe
Julius Decena <julius.decena3095@gmail.com>
2023-04-02 16:09:24 +0800

    updated sales report

--- app/Exports/InventoryExport.php
+++ app/Exports/InventoryExport.php
@@ -17,17 +17,19 @@ class InventoryExport implements FromCollection, WithMapping, WithHeadings
      */
     public function collection()
     {
-        $result = TransactionDetail::from( 'transaction_details as td')
-        ->select(DB::raw('td.item_id as id, i.name as item_name, b.name as brand_name, sum(case when (th.transaction_type_id = 1 or th.transaction_type_id = 4) then td.quantity else 0 end) as incoming, sum(case when (th.transaction_type_id = 2 or th.transaction_type_id = 3) then td.quantity else 0 end) as outgoing, sum(case when (th.transaction_type_id = 1 or th.transaction_type_id = 4) then td.quantity else 0 end) - sum(case when (th.transaction_type_id = 2 or th.transaction_type_id = 3) then td.quantity else 0 end) as current_inventory, min(th.updated_at) as date_received, max(th.updated_at) as last_update'))
-        ->leftJoin('transaction_headers as th', 'th.id', '=', 'td.transaction_header_id')
-        ->where('th.status', '=', 1)
-        ->where('th.branch_id', '=', app('user_branch_id'))
-        ->join('items as i', 'i.id', '=', 'td.item_id')
-        ->join('brands as b', 'i.brand_id', '=', 'b.id')
-        ->groupBy('td.item_id')
-        ->get();
+        $query = TransactionDetail::from( 'transaction_details as td');
+        $query->select(DB::raw('td.item_id as id, td.qr_code as qr_code, i.name as item_name, b.name as brand_name, sum(case when (th.transaction_type_id = 1 or th.transaction_type_id = 4) then td.quantity else 0 end) as incoming, sum(case when (th.transaction_type_id = 2 or th.transaction_type_id = 3) then td.quantity else 0 end) as outgoing, sum(case when (th.transaction_type_id = 1 or th.transaction_type_id = 4) then td.quantity else 0 end) - sum(case when (th.transaction_type_id = 2 or th.transaction_type_id = 3) then td.quantity else 0 end) as current_inventory, min(th.updated_at) as date_received, max(th.updated_at) as last_update'));
 
-        return $result;
+        $query->where('th.branch_id', app('user_branch_id'));
+        $query->where('th.status', 1);
+        $query->with(['item']);
+        $query->with(['item.brand']);
+        $query->join('transaction_headers as th', 'th.id', '=', 'td.transaction_header_id');
+        $query->join('items as i', 'i.id', '=', 'td.item_id');
+        $query->join('brands as b', 'i.brand_id', '=', 'b.id');
+        $query->groupBy('td.qr_code');
+
+        return $query->get();
     }
 
     /**
@@ -36,6 +38,7 @@ class InventoryExport implements FromCollection, WithMapping, WithHeadings
     public function headings(): array
     {
         return [
+            'QR Code',
             'Brand',
             'Item',
             'Date Received',
@@ -54,6 +57,7 @@ class InventoryExport implements FromCollection, WithMapping, WithHeadings
     public function map($result): array
     {
         return [
+            $result->qr_code,
             $result->brand_name,
             $result->item_name,
             $result->date_received,
