9e50b4798160f805f465d0359dd1938cf3dc96db
Julius Decena <julius.decena3095@gmail.com>
2023-06-12 01:07:54 +0800

    Update income-headers.blade.php

--- resources/views/admin/expense/income-headers.blade.php
+++ resources/views/admin/expense/income-headers.blade.php
@@ -1,11 +1,11 @@
 <thead>
     <tr>
         {{-- <th width="5%">&nbsp;</th> --}}
-        <th width="25%">Particulars / Ref. #</th>
+        <th width="20%">Particulars / Ref. #</th>
         <th class="text-right" width="10%">Amount</th>
         <th width="10%">Category</th>
-        <th width="5%">Branch</th>
-        <th width="25%">Remarks</th>
+        <th width="10%">Branch</th>
+        <th width="20%">Remarks</th>
         <th width="10%">Date</th>
     </tr>
 </thead>
